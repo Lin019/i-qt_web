5fe8dcc39de816b6bbce12d785ed67327999466c
DAVIDLIN\DavidLin <voyage01090@gmail.com>
2017-07-26 17:10:35 +0800

    add unit test.

---
+++ UnitTestProject1/RWDtestControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication2.Controllers;
+using WebApplication2;
+using WebApplication2.Models;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RWDtestControllerTest
+    {
+        [TestMethod]
+        public void GetExample()
+        {
+            RWDtestController controller = new RWDtestController();
+
+            JsonResult result = controller.GetExample() as JsonResult;
+
+            dynamic data = result.Data;
+
+            Ticket testTicket = new Ticket
+            {
+                TicketId = 6,
+                From = "Paris",
+                To = "Copenhagen",
+                Departure = Convert.ToDateTime("2017/07/24"),
+                Return = Convert.ToDateTime("2017/07/30"),
+                Adult = 2,
+                Child = 0
+            };
+            Assert.IsNotNull(result);
+            Assert.AreEqual(testTicket.TicketId, data.TicketId);
+        }
+    }
+}
