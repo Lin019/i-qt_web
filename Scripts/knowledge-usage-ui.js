0479d19197f2513254400b2a7f4f35c63e0c9f51
DAVIDLIN\DavidLin <voyage01090@gmail.com>
2017-08-01 17:54:04 +0800

    option sticky footer in fiv.chat (WIP) .

--- Scripts/knowledge-usage-ui.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    var height = $(window).height();
-    $(".logbox div.logbox-body").height(height - 109);
-
-    $(window).resize(function () {
-        var height = $(window).height();
-        $(".logbox div.logbox-body").height(height - 109);
-    });
-});
--- WebApplication2/Scripts/test.js
+++ WebApplication2/Scripts/test.js
@@ -1,5 +1,12 @@
 ﻿
+/*
+    if user input = search  ->  reply & toggle options on  ->  user click one of the option  ->  reply form
+    if user input = form    ->  reply form
+    else                    ->  reply the same as input
+*/
 $(function () {
+
+    $(".wapper").height = $(".chat").height();
 
     $("#home").click(function () {
         window.location.href = 'SynonymCreate.html';
@@ -15,7 +22,7 @@ $(function () {
 
         if ($(".textbox").val() != "") {
 
-            if ($("ul li p").last().hasClass("saving")) {
+            if ($("ul li.chat-reply p").last().hasClass("saving")) {
                 $("ul li.chat-user:after").last().insertAfter(innerHtml);
             }
             else {
@@ -23,7 +30,7 @@ $(function () {
             }
             $(".chat .chat-user p").last().text(inputText);            
             $(".textbox").val("");
-            $(".chat").scrollTop($(".chat ul").height());
+            scrollToBottom();
 
             loading();
 
@@ -36,6 +43,7 @@ $(function () {
         }
     });
 
+    //reply loading animation
     function loading() {
         var replyHtml = "<li class =\"chat-reply\"><span class=\"circle\"></span></li>"
 
@@ -43,11 +51,12 @@ $(function () {
 
         setTimeout(function () {
             $("ul").append(replyHtml);
-            $("ul li").last().append(loadingHtml);
-            $(".chat").scrollTop($(".chat ul").height());
+            $("ul li.chat-reply").last().append(loadingHtml);
+            scrollToBottom();
         }, 1000);
     }
 
+    //reply after user clicked one of the option
     function replyForm(food) {
         var replyHtml = "<form><table></table></form>"
 
@@ -56,16 +65,17 @@ $(function () {
             $("ul li .bubble-reply").last().append(replyHtml);
 
             if (food == "coffee")
-                $(".chat table").last().html("<img src=\"/Images/food_coffee_beans.jpg\"><a href=\"#\">check</a>");
+                $(".chat table").last().html("<tr><td><img src=\"Images/food_coffee_beans.jpg\"></td></tr><tr><td><a href=\"#\">check</a></td></tr><tr><td><a href=\"#\">cancel</a></td></tr>");
             else if (food == "strawberry")
-                $(".chat table").last().html("<img src=\"/Images/food_strawberry.jpg\"><a href=\"#\">check</a>");
+                $(".chat table").last().html("<tr><td><img src=\"Images/food_strawberry.jpg\"></td></tr><tr><td><a href=\"#\">check</a></td></tr><tr><td><a href=\"#\">cancel</a></td></tr>");
             else
-                $(".chat table").last().html("<img src=\"/Images/food_bread.jpg\"><a href=\"#\">check</a>");
+                $(".chat table").last().html("<tr><td><img src=\"Images/food_bread.jpg\"></td></tr><tr><td><a href=\"#\">check</a></td></tr><tr><td><a href=\"#\">cancel</a></td></tr>");
 
-            $(".chat").scrollTop($(".chat ul").height());
+            scrollToBottom();
         }, 3000);
     }
 
+    //reply the same as user
     function reply(replyText) {
         var replyHtml = "<p></p>"
 
@@ -73,12 +83,12 @@ $(function () {
             $("ul li .bubble-reply").last().html("");
             $("ul li .bubble-reply").last().append(replyHtml);
             $(".chat p").last().text(replyText);
-            $(".chat").scrollTop($(".chat ul").height());
+            scrollToBottom();
 
         }, 3000);
-
     }
 
+    //reply if user enter "search"
     function replySearch() {
         var replyHtml = "<p></p>"
 
@@ -88,10 +98,11 @@ $(function () {
             $(".chat p").last().text("請問你要查些什麼呢？");
             toggleOption(true);
             adjustChatHeight();
-            $(".chat").scrollTop($(".chat ul").height());
+            scrollToBottom();
         }, 3000);
     }
 
+    //click at one of the option
     if ($(".option a").click(function () {
 
         var innerHtml = "<li class =\"chat-user\"><span class=\"bubble\"><p>hi</p></span></li>"
@@ -101,12 +112,17 @@ $(function () {
         toggleOption(false);
         adjustChatHeight();
 
-        $(".chat").scrollTop($(".chat ul").height());
+        scrollToBottom();
 
         loading();
         replyForm($(this).text());
     }))
 
+    function scrollToBottom() {
+        $(".chat").scrollTop($(".chat ul").height());
+    }
+
+    //toggle option on or off
     function toggleOption(flag) {
 
         if (flag)
@@ -116,11 +132,12 @@ $(function () {
         }
     }
 
+    //adjust height of the chat area
     function adjustChatHeight() {
         if ($(".option").hasClass("hide"))
-            $(".chat").css("bottom", "51px");
+            $(".chat").css("bottom", "60px");
         else
-            $(".chat").css("bottom", "83px");
+            $(".chat").css("bottom", "60px");
     }
 
     //Prevent submission while pressing Enter key
